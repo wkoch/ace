21e092d3306054a1026bc3bd80295a3c92715853
William Koch <wkochbrg@gmail.com>
2019-03-02 10:41:52 -0300

    Switched to Rollup

---
+++ __sapper__/dev/client/chunk.ed69ed00.js
@@ -0,0 +1,539 @@
+function noop() {}
+
+function assign(tar, src) {
+	for (var k in src) tar[k] = src[k];
+	return tar;
+}
+
+function addLoc(element, file, line, column, char) {
+	element.__svelte_meta = {
+		loc: { file, line, column, char }
+	};
+}
+
+function run(fn) {
+	fn();
+}
+
+function append(target, node) {
+	target.appendChild(node);
+}
+
+function insert(target, node, anchor) {
+	target.insertBefore(node, anchor);
+}
+
+function detachNode(node) {
+	node.parentNode.removeChild(node);
+}
+
+function destroyEach(iterations, detach) {
+	for (var i = 0; i < iterations.length; i += 1) {
+		if (iterations[i]) iterations[i].d(detach);
+	}
+}
+
+function createElement(name) {
+	return document.createElement(name);
+}
+
+function createSvgElement(name) {
+	return document.createElementNS('http://www.w3.org/2000/svg', name);
+}
+
+function createText(data) {
+	return document.createTextNode(data);
+}
+
+function addListener(node, event, handler, options) {
+	node.addEventListener(event, handler, options);
+}
+
+function removeListener(node, event, handler, options) {
+	node.removeEventListener(event, handler, options);
+}
+
+function setAttribute(node, attribute, value) {
+	if (value == null) node.removeAttribute(attribute);
+	else node.setAttribute(attribute, value);
+}
+
+function children (element) {
+	return Array.from(element.childNodes);
+}
+
+function claimElement (nodes, name, attributes, svg) {
+	for (var i = 0; i < nodes.length; i += 1) {
+		var node = nodes[i];
+		if (node.nodeName === name) {
+			for (var j = 0; j < node.attributes.length; j += 1) {
+				var attribute = node.attributes[j];
+				if (!attributes[attribute.name]) node.removeAttribute(attribute.name);
+			}
+			return nodes.splice(i, 1)[0]; // TODO strip unwanted attributes
+		}
+	}
+
+	return svg ? createSvgElement(name) : createElement(name);
+}
+
+function claimText (nodes, data) {
+	for (var i = 0; i < nodes.length; i += 1) {
+		var node = nodes[i];
+		if (node.nodeType === 3) {
+			node.data = data;
+			return nodes.splice(i, 1)[0];
+		}
+	}
+
+	return createText(data);
+}
+
+function setData(text, data) {
+	text.data = '' + data;
+}
+
+function linear(t) {
+	return t;
+}
+
+function generateRule({ a, b, delta, duration }, ease, fn) {
+	const step = 16.666 / duration;
+	let keyframes = '{\n';
+
+	for (let p = 0; p <= 1; p += step) {
+		const t = a + delta * ease(p);
+		keyframes += p * 100 + `%{${fn(t, 1 - t)}}\n`;
+	}
+
+	return keyframes + `100% {${fn(b, 1 - b)}}\n}`;
+}
+
+// https://github.com/darkskyapp/string-hash/blob/master/index.js
+function hash(str) {
+	let hash = 5381;
+	let i = str.length;
+
+	while (i--) hash = ((hash << 5) - hash) ^ str.charCodeAt(i);
+	return hash >>> 0;
+}
+
+function wrapTransition(component, node, fn, params, intro) {
+	let obj = fn.call(component, node, params);
+	let duration;
+	let ease;
+	let cssText;
+
+	let initialised = false;
+
+	return {
+		t: intro ? 0 : 1,
+		running: false,
+		program: null,
+		pending: null,
+
+		run(b, callback) {
+			if (typeof obj === 'function') {
+				transitionManager.wait().then(() => {
+					obj = obj();
+					this._run(b, callback);
+				});
+			} else {
+				this._run(b, callback);
+			}
+		},
+
+		_run(b, callback) {
+			duration = obj.duration || 300;
+			ease = obj.easing || linear;
+
+			const program = {
+				start: window.performance.now() + (obj.delay || 0),
+				b,
+				callback: callback || noop
+			};
+
+			if (intro && !initialised) {
+				if (obj.css && obj.delay) {
+					cssText = node.style.cssText;
+					node.style.cssText += obj.css(0, 1);
+				}
+
+				if (obj.tick) obj.tick(0, 1);
+				initialised = true;
+			}
+
+			if (!b) {
+				program.group = outros.current;
+				outros.current.remaining += 1;
+			}
+
+			if (obj.delay) {
+				this.pending = program;
+			} else {
+				this.start(program);
+			}
+
+			if (!this.running) {
+				this.running = true;
+				transitionManager.add(this);
+			}
+		},
+
+		start(program) {
+			component.fire(`${program.b ? 'intro' : 'outro'}.start`, { node });
+
+			program.a = this.t;
+			program.delta = program.b - program.a;
+			program.duration = duration * Math.abs(program.b - program.a);
+			program.end = program.start + program.duration;
+
+			if (obj.css) {
+				if (obj.delay) node.style.cssText = cssText;
+
+				const rule = generateRule(program, ease, obj.css);
+				transitionManager.addRule(rule, program.name = '__svelte_' + hash(rule));
+
+				node.style.animation = (node.style.animation || '')
+					.split(', ')
+					.filter(anim => anim && (program.delta < 0 || !/__svelte/.test(anim)))
+					.concat(`${program.name} ${program.duration}ms linear 1 forwards`)
+					.join(', ');
+			}
+
+			this.program = program;
+			this.pending = null;
+		},
+
+		update(now) {
+			const program = this.program;
+			if (!program) return;
+
+			const p = now - program.start;
+			this.t = program.a + program.delta * ease(p / program.duration);
+			if (obj.tick) obj.tick(this.t, 1 - this.t);
+		},
+
+		done() {
+			const program = this.program;
+			this.t = program.b;
+
+			if (obj.tick) obj.tick(this.t, 1 - this.t);
+
+			component.fire(`${program.b ? 'intro' : 'outro'}.end`, { node });
+
+			if (!program.b && !program.invalidated) {
+				program.group.callbacks.push(() => {
+					program.callback();
+					if (obj.css) transitionManager.deleteRule(node, program.name);
+				});
+
+				if (--program.group.remaining === 0) {
+					program.group.callbacks.forEach(run);
+				}
+			} else {
+				if (obj.css) transitionManager.deleteRule(node, program.name);
+			}
+
+			this.running = !!this.pending;
+		},
+
+		abort(reset) {
+			if (this.program) {
+				if (reset && obj.tick) obj.tick(1, 0);
+				if (obj.css) transitionManager.deleteRule(node, this.program.name);
+				this.program = this.pending = null;
+				this.running = false;
+			}
+		},
+
+		invalidate() {
+			if (this.program) {
+				this.program.invalidated = true;
+			}
+		}
+	};
+}
+
+let outros = {};
+
+function groupOutros() {
+	outros.current = {
+		remaining: 0,
+		callbacks: []
+	};
+}
+
+var transitionManager = {
+	running: false,
+	transitions: [],
+	bound: null,
+	stylesheet: null,
+	activeRules: {},
+	promise: null,
+
+	add(transition) {
+		this.transitions.push(transition);
+
+		if (!this.running) {
+			this.running = true;
+			requestAnimationFrame(this.bound || (this.bound = this.next.bind(this)));
+		}
+	},
+
+	addRule(rule, name) {
+		if (!this.stylesheet) {
+			const style = createElement('style');
+			document.head.appendChild(style);
+			transitionManager.stylesheet = style.sheet;
+		}
+
+		if (!this.activeRules[name]) {
+			this.activeRules[name] = true;
+			this.stylesheet.insertRule(`@keyframes ${name} ${rule}`, this.stylesheet.cssRules.length);
+		}
+	},
+
+	next() {
+		this.running = false;
+
+		const now = window.performance.now();
+		let i = this.transitions.length;
+
+		while (i--) {
+			const transition = this.transitions[i];
+
+			if (transition.program && now >= transition.program.end) {
+				transition.done();
+			}
+
+			if (transition.pending && now >= transition.pending.start) {
+				transition.start(transition.pending);
+			}
+
+			if (transition.running) {
+				transition.update(now);
+				this.running = true;
+			} else if (!transition.pending) {
+				this.transitions.splice(i, 1);
+			}
+		}
+
+		if (this.running) {
+			requestAnimationFrame(this.bound);
+		} else if (this.stylesheet) {
+			let i = this.stylesheet.cssRules.length;
+			while (i--) this.stylesheet.deleteRule(i);
+			this.activeRules = {};
+		}
+	},
+
+	deleteRule(node, name) {
+		node.style.animation = node.style.animation
+			.split(', ')
+			.filter(anim => anim && anim.indexOf(name) === -1)
+			.join(', ');
+	},
+
+	wait() {
+		if (!transitionManager.promise) {
+			transitionManager.promise = Promise.resolve();
+			transitionManager.promise.then(() => {
+				transitionManager.promise = null;
+			});
+		}
+
+		return transitionManager.promise;
+	}
+};
+
+function getSpreadUpdate(levels, updates) {
+	var update = {};
+
+	var to_null_out = {};
+	var accounted_for = {};
+
+	var i = levels.length;
+	while (i--) {
+		var o = levels[i];
+		var n = updates[i];
+
+		if (n) {
+			for (var key in o) {
+				if (!(key in n)) to_null_out[key] = 1;
+			}
+
+			for (var key in n) {
+				if (!accounted_for[key]) {
+					update[key] = n[key];
+					accounted_for[key] = 1;
+				}
+			}
+
+			levels[i] = n;
+		} else {
+			for (var key in o) {
+				accounted_for[key] = 1;
+			}
+		}
+	}
+
+	for (var key in to_null_out) {
+		if (!(key in update)) update[key] = undefined;
+	}
+
+	return update;
+}
+
+function blankObject() {
+	return Object.create(null);
+}
+
+function destroy(detach) {
+	this.destroy = noop;
+	this.fire('destroy');
+	this.set = noop;
+
+	this._fragment.d(detach !== false);
+	this._fragment = null;
+	this._state = {};
+}
+
+function destroyDev(detach) {
+	destroy.call(this, detach);
+	this.destroy = function() {
+		console.warn('Component was already destroyed');
+	};
+}
+
+function _differs(a, b) {
+	return a != a ? b == b : a !== b || ((a && typeof a === 'object') || typeof a === 'function');
+}
+
+function fire(eventName, data) {
+	var handlers =
+		eventName in this._handlers && this._handlers[eventName].slice();
+	if (!handlers) return;
+
+	for (var i = 0; i < handlers.length; i += 1) {
+		var handler = handlers[i];
+
+		if (!handler.__calling) {
+			try {
+				handler.__calling = true;
+				handler.call(this, data);
+			} finally {
+				handler.__calling = false;
+			}
+		}
+	}
+}
+
+function flush(component) {
+	component._lock = true;
+	callAll(component._beforecreate);
+	callAll(component._oncreate);
+	callAll(component._aftercreate);
+	component._lock = false;
+}
+
+function get() {
+	return this._state;
+}
+
+function init(component, options) {
+	component._handlers = blankObject();
+	component._slots = blankObject();
+	component._bind = options._bind;
+	component._staged = {};
+
+	component.options = options;
+	component.root = options.root || component;
+	component.store = options.store || component.root.store;
+
+	if (!options.root) {
+		component._beforecreate = [];
+		component._oncreate = [];
+		component._aftercreate = [];
+	}
+}
+
+function on(eventName, handler) {
+	var handlers = this._handlers[eventName] || (this._handlers[eventName] = []);
+	handlers.push(handler);
+
+	return {
+		cancel: function() {
+			var index = handlers.indexOf(handler);
+			if (~index) handlers.splice(index, 1);
+		}
+	};
+}
+
+function set(newState) {
+	this._set(assign({}, newState));
+	if (this.root._lock) return;
+	flush(this.root);
+}
+
+function _set(newState) {
+	var oldState = this._state,
+		changed = {},
+		dirty = false;
+
+	newState = assign(this._staged, newState);
+	this._staged = {};
+
+	for (var key in newState) {
+		if (this._differs(newState[key], oldState[key])) changed[key] = dirty = true;
+	}
+	if (!dirty) return;
+
+	this._state = assign(assign({}, oldState), newState);
+	this._recompute(changed, this._state);
+	if (this._bind) this._bind(changed, this._state);
+
+	if (this._fragment) {
+		this.fire("state", { changed: changed, current: this._state, previous: oldState });
+		this._fragment.p(changed, this._state);
+		this.fire("update", { changed: changed, current: this._state, previous: oldState });
+	}
+}
+
+function _stage(newState) {
+	assign(this._staged, newState);
+}
+
+function setDev(newState) {
+	if (typeof newState !== 'object') {
+		throw new Error(
+			this._debugName + '.set was called without an object of data key-values to update.'
+		);
+	}
+
+	this._checkReadOnly(newState);
+	set.call(this, newState);
+}
+
+function callAll(fns) {
+	while (fns && fns.length) fns.shift()();
+}
+
+function _mount(target, anchor) {
+	this._fragment[this._fragment.i ? 'i' : 'm'](target, anchor || null);
+}
+
+var protoDev = {
+	destroy: destroyDev,
+	get,
+	fire,
+	on,
+	set: setDev,
+	_recompute: noop,
+	_set,
+	_stage,
+	_mount,
+	_differs
+};
+
+export { assign as a, protoDev as b, init as c, createElement as d, claimElement as e, children as f, detachNode as g, addLoc as h, insert as i, getSpreadUpdate as j, flush as k, createText as l, claimText as m, append as n, setData as o, addListener as p, setAttribute as q, wrapTransition as r, removeListener as s, groupOutros as t, run as u, destroyEach as v };

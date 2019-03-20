0b9163f433671ba750644b9afaf355e84c722969
William Koch <wkochbrg@gmail.com>
2019-03-20 10:01:34 -0300

    adding tests

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,19 +1,22 @@
-describe('Sapper template app', () => {
+describe('ACE empty index', () => {
 	beforeEach(() => {
 		cy.visit('/')
 	});
 
-	it('has the correct <h1>', () => {
-		cy.contains('h1', 'Great success!')
+	it('tem o título da tabela correto', () => {
+		cy.contains('th', 'Linha'),
+		cy.contains('th', 'Tipo'),
+		cy.contains('th', 'Hora'),
+		cy.contains('th', 'Excluir')
 	});
 
-	it('navigates to /about', () => {
-		cy.get('nav a').contains('about').click();
-		cy.url().should('include', '/about');
-	});
+	// it('adiciona uma vistoria normal', () => {
+	// 	cy.get('nav a').contains('about').click();
+	// 	cy.url().should('include', '/about');
+	// });
 
-	it('navigates to /blog', () => {
-		cy.get('nav a').contains('blog').click();
-		cy.url().should('include', '/blog');
-	});
+	// it('navigates to /blog', () => {
+	// 	cy.get('nav a').contains('blog').click();
+	// 	cy.url().should('include', '/blog');
+	// });
 });

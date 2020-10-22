e42bad0e37183d9e9c84ff49048a926a3993518e
William Koch <wkoch@users.noreply.github.com>
2020-10-22 00:16:04 -0300

    Inserts the exported css

--- postcss.config.js
+++ postcss.config.js
@@ -9,6 +9,6 @@ module.exports = {
   plugins: [
     tailwindcss("./tailwind.config.js"),
 
-    ...([]) // TODO FIXME Replace by [purgecss] to get smaller CSS file
+    ...([purgecss]) // TODO FIXME Replace by [purgecss] to get smaller CSS file
   ]
 };
